7d1357cb4986eab5ffe6a50bc459254de01e0a1f
Lamia Haider <lamia.haider@gmail.com>
2018-04-15 23:01:24 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,8 @@
-def hello_t
-  ["Tim", "Tom", "Jim"] each do |name|
+def hello_t(array)
+
+
+  # call your method here!
+  hello_t(["Tim", "Tom", "Jim"]) {|name|
     if name.start_with?("T")
       puts "Hi, #{name}"
     end
-  end
-end
